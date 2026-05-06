5cb19257a5ac58c3cbe715ccf5810248c52f403e
Reniel Glenn Aniora <reniela97@gmail.com>
2026-05-07 02:09:15 +0800

    try

--- db_config.php
+++ db_config.php
@@ -1,5 +1,9 @@
 <?php
-$servername = "mainline.proxy.rlwy.net";
+
+$servername = "ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);mainline.proxy.rlwy.net";
+
 $username = "root";
 $password = "imong_railway_password_diri"; // I-paste ang tinuod nga password gikan sa Railway
 $dbname = "railway";
