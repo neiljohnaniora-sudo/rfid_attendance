7b2a9a7ccb50a8a3b1ab9f6c51f540a3f3bbfb17
Reniel Glenn Aniora <reniela97@gmail.com>
2026-05-07 02:31:15 +0800

    kapoya

--- connection.php
+++ connection.php
@@ -1,11 +1,12 @@
 <?php
 
-$servername = "ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);mainline.proxy.rlwy.net";
+error_reporting(E_ALL);
 
+$servername = "mainline.proxy.rlwy.net";
 $username = "root";
-$password = "bIadgPoRRsOhYqzVKiXrDIONEROJgJnm"; // I-paste ang tinuod nga password gikan sa Railway
+$password = "imong_railway_password_diri"; // I-paste ang tinuod nga password gikan sa Railway
 $dbname = "railway";
 $port = 57930;
 
