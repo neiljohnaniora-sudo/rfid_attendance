1a6fc7af0266282f9a3c9a57c20174e3d93b7498
Reniel Glenn Aniora <reniela97@gmail.com>
2026-05-08 01:58:22 +0800

    failed

--- connection.php
+++ connection.php
@@ -3,12 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Dili na nato i-hardcode, getenv() na atong gamiton
-$servername = getenv('MYSQLHOST');
-$username   = getenv('MYSQLUSER');
-$password   = getenv('MYSQLPASSWORD');
-$dbname     = getenv('MYSQLDATABASE');
-$port       = getenv('MYSQLPORT');
+// Pagkuha sa env variables, unya naay fallback values kung walay sulod
+$servername = getenv('MYSQLHOST') ?: "mainline.proxy.rlwy.net";
+$username   = getenv('MYSQLUSER') ?: "root";
+$password   = getenv('MYSQLPASSWORD') ?: "bIadgPoRRsOhYqzVKiXrDIONEROJgJnm";
+$dbname     = getenv('MYSQLDATABASE') ?: "railway";
+$port       = getenv('MYSQLPORT') ?: 57930;
 
 // Paghimo ug connection gamit ang mga variables
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
