bc457c28b88a7daab8fed1342b26b9861b20f0a9
neiljohnaniora-sudo <https://github.com/neiljohnaniora-sudo/rfid_attendance.git>
2026-05-30 20:25:43 +0800

    para sure

--- connection.php
+++ connection.php
@@ -3,11 +3,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = getenv('MYSQLHOST') ?: "zephyr.proxy.rlwy.net";
-$username   = getenv('MYSQLUSER') ?: "root";
-$password   = getenv('MYSQLPASSWORD') ?: "kjQlEasVoQranHLlCyIowNZcfJmCoHEJ";
-$dbname     = getenv('MYSQLDATABASE') ?: "railway";
-$port       = getenv('MYSQLPORT') ?: 3306;
+$servername = getenv('MYSQLHOST');
+$username   = getenv('MYSQLUSER');
+$password   = getenv('MYSQLPASSWORD');
+$dbname     = getenv('MYSQLDATABASE');
+$port       = getenv('MYSQLPORT');
 
 // Paghimo ug connection gamit ang mga variables
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
