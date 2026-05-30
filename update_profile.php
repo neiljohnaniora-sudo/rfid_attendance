4be83aaff975456e013715025e0a50febab33c33
neiljohnaniora-sudo <https://github.com/neiljohnaniora-sudo/rfid_attendance.git>
2026-05-31 02:32:50 +0800

    anooo

--- update_profile.php
+++ update_profile.php
@@ -6,16 +6,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_SESSION['admin_id'])) {
     $id = $_SESSION['admin_id'];
     $name = $_POST['full_name'];
     $email = $_POST['email'];
+    $institutional_email = $_POST['institutional_email'];
     $phone = $_POST['phone'];
     $address = $_POST['address'];
 
     // I-update ang database
-    $sql = "UPDATE admins SET full_name='$name', email='$email', phone='$phone', address='$address' WHERE id=$id";
+    $sql = "UPDATE admins SET full_name='$name', email='$email', institutional_email='$institutional_email', phone='$phone', address='$address' WHERE id=$id";
     
     if ($conn->query($sql) === TRUE) {
         // I-update usab ang Session para mo-bag-o diretso sa screen
         $_SESSION['admin_name'] = $name;
         $_SESSION['admin_email'] = $email;
+        $_SESSION['admin_institutional_email'] = $institutional_email;
         $_SESSION['admin_phone'] = $phone;
         $_SESSION['admin_address'] = $address;
         
